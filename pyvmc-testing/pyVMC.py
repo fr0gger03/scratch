79a1fec76ed9828fd843977492fe65cad928f757
Tom Twyman <ttwyman@vmware.com>
2021-09-15 15:14:21 -0400

    updated function for getSDDCT0PrefixLists - eliminate display of system created prefix lists
    Signed-off-by: Tom Twyman <ttwyman@vmware.com>

--- pyvmc-testing/pyVMC.py
+++ pyvmc-testing/pyVMC.py
@@ -1515,32 +1515,42 @@ def getSDDCT0PrefixLists(csp_url, session_token):
     if response.status_code == 200:
         json_response = response.json()
         prefixlists = json_response['results']
-
-        for prefixlist in prefixlists:
-            prefixlisttable = PrettyTable(['ID','Display Name','Description'])
-            prefixlisttable.add_row([prefixlist["id"],prefixlist["display_name"],prefixlist["description"]])
-            print("PREFIX:")
-            print(prefixlisttable)
-            prefixtable = PrettyTable(['Sequence','Network','Comparison', 'Action'])
-            i = 0
-            if prefixlist.get('prefixes'): 
-                for prefix in prefixlist['prefixes']:
-                    i+=1
-                    if prefix.get('ge'):
-                        comparison = "ge (greater-than-or-equal)"
-                    elif prefix.get('le'):
-                        comparison = "le (less-than-or-equal)"
-                    else:
-                        comparison = '-'
-                    prefixtable.add_row([i,prefix['network'],comparison,prefix['action']])
-                print(f'PREFIX ENTRIES FOR {prefixlist["id"]}:')
-                print(prefixtable)
-                print("")
-
-        if len(sys.argv) == 3:
-            if sys.argv[2] == "showjson":
-                print('RAW JSON:')
-                print(json.dumps(prefixlists,indent=2))
+#   clear results for any prefix lists found that contain "System created prefix list"
+#   this will return empty dictionaries for any containing the above string
+        for prefix in prefixlists:
+            if prefix['description'].__contains__('System created prefix list'):
+                prefix.clear()
+#   remove empty dictionaries
+        while {} in prefixlists:
+            prefixlists.remove({})
+#   print a nicely formatted list of only user-uploaded prefix lists; system created lists were eliminated in above code
+        if len(prefixlists) != 0: 
+            for prefixlist in prefixlists:
+                prefixlisttable = PrettyTable(['ID','Display Name','Description'])
+                prefixlisttable.add_row([prefixlist["id"],prefixlist["display_name"],prefixlist["description"]])
+                print("PREFIX:")
+                print(prefixlisttable)
+                prefixtable = PrettyTable(['Sequence','Network','Comparison', 'Action'])
+                i = 0
+                if prefixlist.get('prefixes'): 
+                    for prefix in prefixlist['prefixes']:
+                        i+=1
+                        if prefix.get('ge'):
+                            comparison = "ge (greater-than-or-equal)"
+                        elif prefix.get('le'):
+                            comparison = "le (less-than-or-equal)"
+                        else:
+                            comparison = '-'
+                        prefixtable.add_row([i,prefix['network'],comparison,prefix['action']])
+                    print(f'PREFIX ENTRIES FOR {prefixlist["id"]}:')
+                    print(prefixtable)
+                    print("")
+            if len(sys.argv) == 3:
+                if sys.argv[2] == "showjson":
+                    print('RAW JSON:')
+                    print(json.dumps(prefixlists,indent=2))
+        else:
+            print("No prefixes shown - only system created prefixes are found.")
     else:
         print (f'API call failed with status code {response.status_code}. URL: {myURL}.')
 
@@ -1664,6 +1674,9 @@ def attachSDDCT0PrefixList (csp_url, session_token):
             del neighbor['_protection']
             del neighbor['_revision']
 #        print(bgp_neighbors)
+#   obtain list of prefix filters already uploaded
+#   obtain list of current BGP neighbors
+#   
 
         bgp_table = PrettyTable(['ID','Remote AS Num','Remote Address'])
         for neighbor in bgp_neighbors:
@@ -1686,15 +1699,17 @@ def attachSDDCT0PrefixList (csp_url, session_token):
             print("FILTERS:")
             print (filter_table)
 
-#   ask for user input to select which neighbor
-
-
         if len(sys.argv) == 3:
             if sys.argv[2] == "showjson":
                 print('RAW JSON:')
                 print(json.dumps(bgp_neighbors,indent=2))
     else:
         print (f'API call failed with status code {response.status_code}. URL: {myURL}.')
+
+#   ask for user input to select which neighbor
+#   ask for user input to select a filter to attach
+#   ask for user input to select "in_route_filter" or "out_route_filter (default)"
+#   dict['key3'] = 'geeks'
 
 def getSDDCT0routes(proxy_url, session_token):
     myHeader = {'csp-auth-token': session_token}
